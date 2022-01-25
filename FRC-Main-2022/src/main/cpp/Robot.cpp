13a07b5705d015e82ca9a5358135bde31df64e4e
Lenovo Coder <daniel.andresen@okcrobot.com>
2022-01-24 20:58:28 -0600

    Addded our jerry rigged libraries for the

--- FRC-Main-2022/src/main/cpp/Robot.cpp
+++ FRC-Main-2022/src/main/cpp/Robot.cpp
@@ -2,69 +2,41 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <frc/Joystick.h>
-#include <frc/TimedRobot.h>
-#include <frc/drive/MecanumDrive.h>
-#include <frc/motorcontrol/PWMSparkMax.h>
-#include <frc/ADIS16470_IMU.h>
-#include <iostream>
-#include <frc/smartdashboard/SmartDashboard.h>
 #include "Robot.h"
-
+#include <frc/TimedRobot.h>
 /**
  * This is a demo program showing how to use Mecanum control with the
  * MecanumDrive class.
  */
-class Robot : public frc::TimedRobot
-{
-public:
-  void RobotInit() override
+  void Robot::RobotInit()
   {
     // Invert the right side motors. You may need to change or remove this to
     // match your robot.
-    m_frontRight.SetInverted(true);
+    m_rearLeft.SetInverted(true);
     m_rearRight.SetInverted(true);
-
-  m_autoChooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_autoChooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  m_yawChooser.SetDefaultOption(kYawDefault, kYawDefault);
-  m_yawChooser.AddOption(kYawXAxis, kYawXAxis);
-  m_yawChooser.AddOption(kYawYAxis, kYawYAxis);
-  frc::SmartDashboard::PutData("Auto Modes", &m_autoChooser);
-  frc::SmartDashboard::PutData("IMUYawAxis", &m_yawChooser);
-  frc::SmartDashboard::PutBoolean("RunCal", false);
-  frc::SmartDashboard::PutBoolean("ConfigCal", false);
-  frc::SmartDashboard::PutBoolean("Reset", false);
-  frc::SmartDashboard::PutBoolean("SetYawAxis", false);
-  frc::SmartDashboard::PutBoolean("SetDecRate", false);
-  frc::SmartDashboard::PutNumber("DecRate", m_decRate);
   }
 
-  void TeleopPeriodic() override
+  void Robot::RobotPeriodic()
+  {
+  frc::SmartDashboard::PutNumber("YawAngle", m_imu.GetAngle());
+  frc::SmartDashboard::PutNumber("XCompAngle", m_imu.GetXComplementaryAngle());
+  frc::SmartDashboard::PutNumber("YCompAngle", m_imu.GetYComplementaryAngle());
+  m_setDecRate = frc::SmartDashboard::GetBoolean("SetDecRate", false);
+  m_decRate = frc::SmartDashboard::GetNumber("DecRate", 4);
+  m_runCal = frc::SmartDashboard::GetBoolean("RunCal", false);
+  m_configCal = frc::SmartDashboard::GetBoolean("ConfigCal", false);
+  m_reset = frc::SmartDashboard::GetBoolean("Reset", false);
+  m_setYawAxis = frc::SmartDashboard::GetBoolean("SetYawAxis", false);
+  m_yawSelected = m_yawChooser.GetSelected();
+  }
+
+  void Robot::TeleopPeriodic()
   {
     /* Use the joystick X axis for lateral movement, Y axis for forward
      * movement, and Z axis for rotation.
      */
     m_robotDrive.DriveCartesian(m_stick.GetX(), m_stick.GetY(), m_stick.GetZ());
   }
-
-private:
-  static constexpr int kFrontLeftChannel = 0;
-  static constexpr int kRearLeftChannel = 1;
-  static constexpr int kFrontRightChannel = 2;
-  static constexpr int kRearRightChannel = 3;
-
-  static constexpr int kJoystickChannel = 0;
-
-  frc::PWMSparkMax m_frontLeft{kFrontLeftChannel};
-  frc::PWMSparkMax m_rearLeft{kRearLeftChannel};
-  frc::PWMSparkMax m_frontRight{kFrontRightChannel};
-  frc::PWMSparkMax m_rearRight{kRearRightChannel};
-  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
-                                 m_rearRight};
-
-  frc::Joystick m_stick{kJoystickChannel};
-};
 
 #ifndef RUNNING_FRC_TESTS
 int main()
--- FRC-Main-2022/src/main/include/Robot.h
+++ FRC-Main-2022/src/main/include/Robot.h
@@ -9,20 +9,25 @@
 
 #include <string>
 
-#include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
 
-#include <frc/ADIS16470_IMU.h>
+#include <frc/Joystick.h>
+#include <frc/TimedRobot.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
+#include <ADIS16470_IMU.h>
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
   void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
+  //void AutonomousInit() override;
+  //void AutonomousPeriodic() override;
+  //void TeleopInit() override;
   void TeleopPeriodic() override;
-  void TestPeriodic() override;
+  //void TestPeriodic() override;
 
  private:
   frc::SendableChooser<std::string> m_autoChooser;
@@ -42,4 +47,19 @@ class Robot : public frc::TimedRobot {
   uint16_t m_decRate = 4;
   bool m_setDecRate = false;
   frc::ADIS16470_IMU::IMUAxis m_yawActiveAxis = frc::ADIS16470_IMU::IMUAxis::kZ;
+  static constexpr int kFrontLeftChannel = 2;
+  static constexpr int kRearLeftChannel = 3;
+  static constexpr int kFrontRightChannel = 0;
+  static constexpr int kRearRightChannel = 1;
+
+  static constexpr int kJoystickChannel = 0;
+
+  frc::PWMSparkMax m_frontLeft{kFrontLeftChannel};
+  frc::PWMSparkMax m_rearLeft{kRearLeftChannel};
+  frc::PWMSparkMax m_frontRight{kFrontRightChannel};
+  frc::PWMSparkMax m_rearRight{kRearRightChannel};
+  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
+                                 m_rearRight};
+
+  frc::Joystick m_stick{kJoystickChannel};
 };
